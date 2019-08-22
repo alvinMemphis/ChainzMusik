752fde8809750f293254b05cb729ed6dd795a583
alvinmemphis <alvinnsongambi9@gmail.com>
2019-08-22 23:11:34 +0300

    overall app theme set

--- app/src/main/java/com/example/alvin/chainzmusic/MainActivity.java
+++ app/src/main/java/com/example/alvin/chainzmusic/MainActivity.java
@@ -3,16 +3,24 @@ package com.example.alvin.chainzmusic;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
-
+    private BottomNavigationViewEx bottomNavigationViewEx;
+private static String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationViewEx=findViewById(R.id.bottom_nav_view);
         init();
     }
-
+    private void initBottomNavigationView(){
+        Log.d(TAG, "initBottomNavigationView: ");
+        bottomNavigationViewEx.enableAnimation(false);
+    }
     private void init() {
         HomeFragment homeFragment=new HomeFragment();
         FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
